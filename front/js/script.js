85d1f83af825c162f360dcd8ae4817c37db622f8
Agathe <agathe.martin@agat.paris>
2022-08-15 06:42:47 +0200

    finalisation affichage liste produits accueil V.0

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,17 @@
-fetch('http://localhost:3000/api/products')
+function printProducts() {
+    fetch('http://localhost:3000/api/products')
     .then((response) => response.json())
     .then((data) => {
-        printProduct(data);
+        for (let i=0; i<data.length; i++) {  
+            document.getElementById('items').innerHTML +='<a href="./product.html?id='+data[i]._id+'">        <article><img src="'+data[i].imageUrl+'" alt="'+data[i].altTxt+'"/><h3 class="productName">'+data[i].name+'</h3><p class="productDescription">'+data[i].description+'</p></article></a>';
+        }    
     })
     .catch(error => alert('Erreur : ' + error));
-
-function printProduct(data) {
-    for (let i=0; i<data.length; i++) {  
-        document.getElementById('items').innerHTML +='<a href="./product.html?id='+data[i]._id+'">        <article><img src="'+data[i].imageUrl+'" alt="'+data[i].altTxt+'"/><h3 class="productName">'+data[i].name+'</h3><p class="productDescription">'+data[i].description+'</p></article></a>';
-    }
 }
 
 
 
 /*
-
- 
-
-
 function afficherProduits (selector) {
     console.log('Affiche les produits dans ' + selector);
 }
