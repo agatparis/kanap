a8a8fae83b88f63882a8020fac2dbf46d38ae53f
Agathe <agathe.martin@agat.paris>
2022-08-15 06:26:24 +0200

    modifs liste produits

--- front/js/script.js
+++ front/js/script.js
@@ -6,14 +6,18 @@ fetch('http://localhost:3000/api/products')
     .catch(error => alert('Erreur : ' + error));
 
 function printProduct(data) {
-    for (let i=0; i<data.length; i++) {        
-        document.getElementById('items').innerHTML +='<a href="./product.html?id='+data[i]._id+'"><article><img src="'+data[i].imageUrl+'" alt="'+data[i].altTxt+'"/><h3 class="productName">'+data[i].name+'</h3><p class="productDescription">'+data[i].description+'</p></article></a>';
+    for (let i=0; i<data.length; i++) {  
+        document.getElementById('items').innerHTML +='<a href="./product.html?id='+data[i]._id+'">        <article><img src="'+data[i].imageUrl+'" alt="'+data[i].altTxt+'"/><h3 class="productName">'+data[i].name+'</h3><p class="productDescription">'+data[i].description+'</p></article></a>';
     }
 }
 
 
 
 /*
+
+ 
+
+
 function afficherProduits (selector) {
     console.log('Affiche les produits dans ' + selector);
 }
